d2b47a0b1f281e3fff3cce741045356949722344
admin <admin@itcast.cn>
2018-11-05 21:36:51 +0800

    把本地工程添加到git

---
+++ git-test/src/com/itheima/git/FistClass.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FistClass {
+    public static void main(String[] args) {
+        System.out.println("我来了");
+        System.out.println("我来了");
+        System.out.println("我来了");
+        System.out.println("我来了");
+        System.out.println("我来了");
+    }
+}
